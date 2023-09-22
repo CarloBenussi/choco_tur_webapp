c3d93d03007257f3fe355ae708f413d56cea2203
Carlo Benussi <carlo.benussi@cerence.com>
2023-09-22 09:48:33 +0200

    Add Spring Boot template

---
+++ src/main/java/com/chocotur/chocotur/ChocoturApplication.java
@@ -0,0 +1,13 @@
+package com.chocotur.chocotur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocoturApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocoturApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chocotur/chocotur/ChocoturApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chocotur.chocotur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocoturApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
